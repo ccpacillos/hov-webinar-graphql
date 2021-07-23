d71352a31a99eb815f38fcd2206bfbee3bbeba2f
ccpacillos <clint.pacillos@gmail.com>
2021-07-24 07:14:44 +0800

    feat(koa): setup koa server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import cors from '@koa/cors';
+import Koa from 'koa';
+import koaBody from 'koa-body';
+
+async function start() {
+  const koa = new Koa();
+
+  koa.use(cors());
+  koa.use(koaBody());
+}
+
+start();
